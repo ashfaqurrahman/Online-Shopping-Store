abb75b4d46b32eafb2d2e0b4b3a0c9769f3f87d1
OPU <ashfaqurrahman28@gmail.com>
2018-12-12 22:44:57 +0600

    Initial Commit

---
+++ OnlineShoppingStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "ashfaqurrahman28@gmail.com";
+        public string MailFromAddress = "ashfaqur.cse@gmail.com";
+        public bool UseSsl = true;
+        public string Username = "ashfaqur.cse@gmail.com";
+        public string Password = "Ash@114431"; // Create your own google app password, In the video I have shown you how to create app password.
+        public string ServerName = "smtp.gmail.com";
+        public int ServerPort = 587;
+    }
+}
---
+++ OnlineShoppingStore.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using OnlineShoppingStore.Domain.Entities;
+using OnlineShoppingStore.WebUI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingStore.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+        public CartController CartController { get; internal set; }
+    }
+}
